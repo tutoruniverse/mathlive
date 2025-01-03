84438e63f6a05dc80b48670b35d4131acd007b48
HarveyNguyen <binhnt07112420@gmail.com>
2025-01-03 10:15:38 +0700

    fix: workaround to show virtual keyboard in iframe as well

--- src/virtual-keyboard/global.ts
+++ src/virtual-keyboard/global.ts
@@ -6,20 +6,21 @@ export { VirtualKeyboard } from './virtual-keyboard';
 export { VirtualKeyboardProxy } from './proxy';
 
 if (isBrowser() && !('mathVirtualKeyboard' in window)) {
-  if (window === window['top']) {
-    // When at the top-level window, mathVirtualKeyboard is a singleton
-    // VirtualKeyboard. Instantiate it during static init, otherwise
-    // mathfields in iFrame will not be able to talk to it until it has been
-    // instantiated (which the client may not do)
-    const kbd = VirtualKeyboard.singleton;
-    Object.defineProperty(window, 'mathVirtualKeyboard', {
-      get: () => kbd,
-    });
-  } else {
-    // When in an iFrame, the mathVirtualKeyboard is a proxy
-    Object.defineProperty(window, 'mathVirtualKeyboard', {
-      get: () => VirtualKeyboardProxy.singleton,
-      configurable: true,
-    });
-  }
+  // if (window === window['top']) {
+  //   // When at the top-level window, mathVirtualKeyboard is a singleton
+  //   // VirtualKeyboard. Instantiate it during static init, otherwise
+  //   // mathfields in iFrame will not be able to talk to it until it has been
+  //   // instantiated (which the client may not do)
+  // } else {
+  //   // When in an iFrame, the mathVirtualKeyboard is a proxy
+  //   Object.defineProperty(window, 'mathVirtualKeyboard', {
+  //     get: () => VirtualKeyboardProxy.singleton,
+  //     configurable: true,
+  //   });
+  // }
+  // Temporary workaround for issue mathkeyboard won't show up in iframe by using VirtualKeyboard singleton in iframe case as well. Might affect use case when there are multiple iframes in an app
+  const kbd = VirtualKeyboard.singleton;
+  Object.defineProperty(window, 'mathVirtualKeyboard', {
+    get: () => kbd,
+  });
 }
